9d442f9dbac42a0a5a6a311fa0683d91cb38321f
smallbrownbike <snscrn@gmail.com>
2017-06-01 13:31:46 -0700

    Added search bar to every page/created separate JS file (app2.js) to hold 200.html JS

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-window.onload = function(){
+window.onload = () => {
 	
 	var container = document.querySelector('.container');
 	var containerFluid = document.querySelector('.container-fluid');
-	var input = document.querySelector('#input');
+	var input = document.querySelector('input');
+	var secondaryInput = document.querySelector('#secondaryInput');
 	var button = document.querySelector('.btn');
 
 	history.replaceState({title: '', content: containerFluid.innerHTML}, 'WikiSearch', '');
 
 	window.onpopstate = (e) => {
-		updateContent(event.state)
+		updateContent(e.state)
 	};
 
-	function reqListener() {  
+	function reqListener() { 
+		history.pushState({title: encodeURI(input.value), content: JSON.parse(this.responseText)}, input.value, input.value);
 		var arr = JSON.parse(this.responseText);
 		showContent(arr);
-		history.pushState({title: encodeURI(input.value), content: arr}, input.value, input.value)
 	};
 
 	function reqError(err) {  
@@ -30,26 +31,32 @@ window.onload = function(){
 	}
 
 	function showContent(data) {
+		
 		container.style.height = '100%';
 		var keys = Object.keys(data.query.pages);
-		var html = '<div class="row">';
+		var html = '<div class="row"><span id="secondaryMag" class="fa fa-search fa-2x"></span><input class="text-primary" id="secondaryInput" autocomplete="off" autofocus>'
 		for (var i = 0; i < keys.length; i++){
 			html += `<a href='https://en.wikipedia.org/?curid=${keys[i]}'><div class='col-lg-12'><h3>${data.query.pages[`${Number(keys[i])}`].title}</h3><h5>${data.query.pages[`${Number(keys[i])}`].extract}</h5></div></a>`;
 		}
 		html += '</div>'
 		containerFluid.innerHTML =  html;
+		input = document.querySelector('input');
 	}
 
-	input.addEventListener('keyup', (e) => {
+	document.addEventListener('keyup', (e) => {
 		if (e.which === 13) {
+			if (input.value !== ''){
+				var url = `https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=${encodeURI(input.value)}`;
 
-			var url = `https://cors-anywhere.herokuapp.com/https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=${encodeURI(input.value)}`;
-
-			var xhr = new XMLHttpRequest();
-			xhr.onload = reqListener;  
-			xhr.onerror = reqError; 
-			xhr.open('get', url);
-			xhr.send();
+				var xhr = new XMLHttpRequest();
+				xhr.onload = reqListener;  
+				xhr.onerror = reqError; 
+				xhr.open('get', url);
+				xhr.send();
+				
+			}
+			
+			
 		}
 	});
 }
